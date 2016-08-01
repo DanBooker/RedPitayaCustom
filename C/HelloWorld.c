77d369e74933c1ee8e91ecf4369e9f65aff07401
DanBooker <djb1g13@soton.ac.uk>
2016-08-01 14:18:46 +0100

    Updating HelloWorld

--- C/HelloWorld.c
+++ C/HelloWorld.c
@@ -1,16 +1,15 @@
 //Learning C++
-#include <iostream>
-using namespace std;
+#include <stdio.h>
 
 int main()
 {
 	int i;
 	int * pntr;
 	pntr = &i;
-	cout << "Please enter an integer value: ";
-	cin >> i;
-	cout << "The value you entered is " << i;
-	cout << " and its double is " << i * 2 << ".\n";
-	cout << "It is located at " << pntr << " in memory.\n";
+	printf "Please enter an integer value: ";
+	fgets(i);
+	printf ("The value you entered is ", i);
+	printf(" and its double is ", i * 2 , ".\n");
+	printf("It is located at " , pntr , " in memory.\n");
 	return 0;
 }
