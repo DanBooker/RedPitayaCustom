b03c93f43f94672c0626cfeb16e36fa154ac361a
DanBooker <djb1g13@soton.ac.uk>
2016-08-01 14:33:46 +0100

    Hello please

--- C/HelloWorld.c
+++ C/HelloWorld.c
@@ -3,13 +3,6 @@
 
 int main()
 {
-	int i;
-	int * pntr;
-	pntr = &i;
-	printf "Please enter an integer value: ";
-	fgets(i);
-	printf ("The value you entered is %d", i);
-	printf(" and its double is %d.\n", i*2);
-	printf("It is located at %d in memory.\n", pntr);
+	printf("Hello World!");
 	return 0;
 }
